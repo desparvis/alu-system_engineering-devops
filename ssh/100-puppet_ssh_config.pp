6ab959e01ec868ae76ab4865a6853a76ca4aeeae
desparvis <latifahm501@gmail.com>
2024-11-02 09:42:20 -0700

    r

--- ssh/100-puppet_ssh_config.pp
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# automation to make changes on configfile
-file { 'etc/ssh/ssh_config':
-  ensure  => present,
-  path    => "/etc/ssh/ssh_config",
-  content => 'IdentityFile ~/.ssh/school\n\tPasswordAuthentication no'}
